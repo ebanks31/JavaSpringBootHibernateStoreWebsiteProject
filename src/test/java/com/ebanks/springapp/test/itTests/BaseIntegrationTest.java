7a3d0e557c62e9bedfd832d4d1ed5a47a731b614
unknown <ericbanks31@gmail.com>
2018-11-23 21:53:21 -0500

    Adding unit tests to source folder in order to build the maven test builds

---
+++ src/test/java/com/ebanks/springapp/test/itTests/BaseIntegrationTest.java
@@ -0,0 +1,13 @@
+package com.ebanks.springapp.test.itTests;
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest()
+@ActiveProfiles("test")
+public abstract class BaseIntegrationTest {
+
+}
